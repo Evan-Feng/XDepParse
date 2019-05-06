f389561cbd9fb1551a6986930b4adc79132124cf
Ubuntu <ubuntu@ip-172-31-1-130.us-west-2.compute.internal>
2019-05-06 11:32:11 +0000

    Modify environment variables

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,10 +4,10 @@
 
 # Set UDBASE to the location of CoNLL18 folder
 # For details, see http://universaldependencies.org/conll18/data.html
-export UDBASE=/u/nlp/data/dependency_treebanks/CoNLL18/
+export UDBASE=~/projects/CoNLL18/
 
 # Set directories to store processed training/evaluation files
-export DATA_ROOT=./data
+export DATA_ROOT=~/projects/stanfordnlp/data
 export TOKENIZE_DATA_DIR=$DATA_ROOT/tokenize
 export MWT_DATA_DIR=$DATA_ROOT/mwt
 export LEMMA_DATA_DIR=$DATA_ROOT/lemma
@@ -16,4 +16,4 @@ export DEPPARSE_DATA_DIR=$DATA_ROOT/depparse
 export ETE_DATA_DIR=$DATA_ROOT/ete
 
 # Set directories to store external word vector data
-export WORDVEC_DIR=./extern_data/word2vec
+export WORDVEC_DIR=~/projects/stanfordnlp/extern_data/word2vec
