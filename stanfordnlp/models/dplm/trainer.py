2ce37253a298d7f947f4acbd8b8677b29dc75388
evanfeng <evanfeng@pku.edu.cn>
2019-05-10 00:37:01 +0000

    Add joint training of parser and language model

---
+++ stanfordnlp/models/dplm/trainer.py
@@ -0,0 +1,116 @@
+"""
+A trainer class to handle training and testing of models.
+"""
+
+import sys
+import torch
+from torch import nn
+import numpy as np
+
+from stanfordnlp.models.common.trainer import Trainer as BaseTrainer
+from stanfordnlp.models.common import utils, loss
+from stanfordnlp.models.common.chuliu_edmonds import chuliu_edmonds_one_root
+from stanfordnlp.models.dplm.model import ParserLM
+from stanfordnlp.models.pos.vocab import MultiVocab
+from stanfordnlp.models.common.rnn_utils import copy_rnn_weights
+from stanfordnlp.models.depparse.trainer import unpack_batch as depparse_unpack
+from stanfordnlp.models.lm.trainer import unpack_batch as lm_unpack
+
+
+class Trainer(BaseTrainer):
+    """ A trainer for training models. """
+
+    def __init__(self, args=None, vocab=None, pretrain=None, model_file=None, use_cuda=False, weight_decay=0):
+        self.use_cuda = use_cuda
+        if model_file is not None:
+            # load everything from file
+            self.load(pretrain, model_file)
+        else:
+            assert all(var is not None for var in [args, vocab, pretrain])
+            # build model from scratch
+            self.args = args
+            self.vocab = vocab
+            self.model = ParserLM(args, vocab, emb_matrix=pretrain.emb)
+        self.parameters = [p for p in self.model.parameters() if p.requires_grad]
+        if self.use_cuda:
+            self.model.cuda()
+        else:
+            self.model.cpu()
+        self.optimizer = utils.get_optimizer(self.args['optim'], self.parameters, self.args['lr'],
+                                             betas=(0.9, self.args['beta2']), eps=1e-6, weight_decay=weight_decay)
+
+    def update(self, dp_batch, lm_batch, eval=False):
+        inputs, orig_idx, word_orig_idx, sentlens, wordlens = depparse_unpack(dp_batch, self.use_cuda)
+        word, word_mask, wordchars, wordchars_mask, upos, xpos, ufeats, pretrained, lemma, head, deprel = inputs
+
+        if eval:
+            self.model.eval()
+        else:
+            self.model.train()
+            self.optimizer.zero_grad()
+
+        depparse_loss, _ = self.model(word, word_mask, wordchars, wordchars_mask, upos, xpos, ufeats,
+                                      pretrained, lemma, head, deprel, word_orig_idx, sentlens, wordlens)
+
+        inputs, orig_idx, word_orig_idx, sentlens, wordlens = lm_unpack(lm_batch, self.use_cuda)
+        word, word_mask, wordchars, wordchars_mask, upos, xpos, ufeats, pretrained, lemma, next_word, prev_word = inputs
+
+        lm_loss, _ = self.model.lm_forward(word, word_mask, wordchars, wordchars_mask, upos, xpos, ufeats,
+                                           pretrained, lemma, next_word, prev_word, word_orig_idx, sentlens, wordlens)
+        loss = depparse_loss * self.args['lambd'] + lm_loss
+        # print(np.exp(lm_loss.item()))
+
+        if not eval:
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.args['max_grad_norm'])
+            self.optimizer.step()
+
+        depparse_loss = depparse_loss.item()
+        lm_loss = lm_loss.item()
+        loss = loss.item()
+
+        return depparse_loss, lm_loss, loss
+
+    def predict(self, batch, unsort=True):
+        inputs, orig_idx, word_orig_idx, sentlens, wordlens = depparse_unpack(batch, self.use_cuda)
+        word, word_mask, wordchars, wordchars_mask, upos, xpos, ufeats, pretrained, lemma, head, deprel = inputs
+
+        self.model.eval()
+        batch_size = word.size(0)
+        _, preds = self.model(word, word_mask, wordchars, wordchars_mask, upos, xpos, ufeats, pretrained, lemma, head, deprel, word_orig_idx, sentlens, wordlens)
+        head_seqs = [chuliu_edmonds_one_root(adj[:l, :l])[1:] for adj, l in zip(preds[0], sentlens)]  # remove attachment for the root
+        deprel_seqs = [self.vocab['deprel'].unmap([preds[1][i][j + 1][h] for j, h in enumerate(hs)]) for i, hs in enumerate(head_seqs)]
+
+        pred_tokens = [[[str(head_seqs[i][j]), deprel_seqs[i][j]] for j in range(sentlens[i] - 1)] for i in range(batch_size)]
+        if unsort:
+            pred_tokens = utils.unsort(pred_tokens, orig_idx)
+        return pred_tokens
+
+    def save(self, filename, skip_modules=True):
+        model_state = self.model.state_dict()
+        # skip saving modules like pretrained embeddings, because they are large and will be saved in a separate file
+        if skip_modules:
+            skipped = [k for k in model_state.keys() if k.split('.')[0] in self.model.unsaved_modules]
+            for k in skipped:
+                del model_state[k]
+        params = {
+            'model': model_state,
+            'vocab': self.vocab.state_dict(),
+            'config': self.args
+        }
+        try:
+            torch.save(params, filename)
+            print("model saved to {}".format(filename))
+        except BaseException:
+            print("[Warning: Saving failed... continuing anyway.]")
+
+    def load(self, pretrain, filename):
+        try:
+            checkpoint = torch.load(filename, lambda storage, loc: storage)
+        except BaseException:
+            print("Cannot load model from {}".format(filename))
+            sys.exit(1)
+        self.args = checkpoint['config']
+        self.vocab = MultiVocab.load_state_dict(checkpoint['vocab'])
+        self.model = Parser(self.args, self.vocab, emb_matrix=pretrain.emb)
+        self.model.load_state_dict(checkpoint['model'], strict=False)
---
+++ stanfordnlp/models/parser_lm.py
@@ -0,0 +1,301 @@
+"""
+Entry point for training and evaluating a dependency parser.
+
+This implementation combines a deep biaffine graph-based parser with linearization and distance features.
+For details please refer to paper: https://nlp.stanford.edu/pubs/qi2018universal.pdf.
+"""
+
+"""
+Training and evaluation for the parser.
+"""
+
+import sys
+import os
+import shutil
+import time
+from datetime import datetime
+import argparse
+import numpy as np
+import random
+import torch
+from torch import nn, optim
+
+from stanfordnlp.models.dplm.trainer import Trainer
+from stanfordnlp.models.lm.data import DataLoader as LMDataLoader
+from stanfordnlp.models.depparse.data import DataLoader as DPDataLoader
+from stanfordnlp.models.depparse import scorer
+from stanfordnlp.models.common import utils
+from stanfordnlp.models.common.pretrain import Pretrain
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='data/depparse', help='Root dir for saving models.')
+    parser.add_argument('--wordvec_dir', type=str, default='extern_data/word2vec', help='Directory of word vectors')
+    parser.add_argument('--train_file', type=str, default=None, help='Input file for data loader.')
+    parser.add_argument('--eval_file', type=str, default=None, help='Input file for data loader.')
+    parser.add_argument('--output_file', type=str, default=None, help='Output CoNLL-U file.')
+    parser.add_argument('--gold_file', type=str, default=None, help='Output CoNLL-U file.')
+    parser.add_argument('--lm_file', type=str, nargs='+', default=None, help='input for langauge modeling')
+
+    # additional arguments
+    parser.add_argument('--vocab_cutoff', type=int, default=7, help='Word frequency threshold for vocab construction')
+    parser.add_argument('--lemma_emb_dim', type=int, default=75)
+    parser.add_argument('--wdecay', type=float, default=1e-6, help='weight decay applied to all weights')
+    parser.add_argument('--lstm_type', type=str, default='wdlstm', choices=['hlstm', 'wdlstm', 'bihlstm'], help="LSTM type")
+    # parser.add_argument('--pretrain_lm', type=str, default=None, help='dir of the pretrained lm (optional)')
+    # parser.add_argument('--finetune', action='store_true', help='finetune all pretrained parameters')
+    parser.add_argument('--lambd', type=float, default=1, help='trade-off between dependency parsing and language modeling')
+
+    parser.add_argument('--mode', default='train', choices=['train', 'predict'])
+    parser.add_argument('--lang', type=str, help='Language')
+    parser.add_argument('--shorthand', type=str, help="Treebank shorthand")
+    parser.add_argument('--scorer', choices=['biaffine', 'mlp'], default='biaffine')
+
+    parser.add_argument('--hidden_dim', type=int, default=400)
+    parser.add_argument('--char_hidden_dim', type=int, default=400)
+    parser.add_argument('--deep_biaff_hidden_dim', type=int, default=400)
+    # parser.add_argument('--composite_deep_biaff_hidden_dim', type=int, default=100)
+    parser.add_argument('--word_emb_dim', type=int, default=75)
+    parser.add_argument('--char_emb_dim', type=int, default=100)
+    parser.add_argument('--tag_emb_dim', type=int, default=50)
+    parser.add_argument('--transformed_dim', type=int, default=125)
+    parser.add_argument('--num_layers', type=int, default=3)
+    parser.add_argument('--char_num_layers', type=int, default=1)
+    parser.add_argument('--word_dropout', type=float, default=0.1)
+    parser.add_argument('--dropout', type=float, default=0.5)
+    parser.add_argument('--rec_dropout', type=float, default=0, help="Recurrent dropout")
+    parser.add_argument('--char_rec_dropout', type=float, default=0, help="Recurrent dropout")
+    parser.add_argument('--no_char', dest='char', action='store_false', help="Turn off character model.")
+    parser.add_argument('--no_pretrain', dest='pretrain', action='store_false', help="Turn off pretrained embeddings.")
+    parser.add_argument('--no_linearization', dest='linearization', action='store_false', help="Turn off linearization term.")
+    parser.add_argument('--no_distance', dest='distance', action='store_false', help="Turn off distance term.")
+
+    parser.add_argument('--sample_train', type=float, default=1.0, help='Subsample training data.')
+    parser.add_argument('--optim', type=str, default='adam', help='sgd, adagrad, adam or adamax.')
+    parser.add_argument('--lr', type=float, default=3e-3, help='Learning rate')
+    parser.add_argument('--beta2', type=float, default=0.95)
+
+    parser.add_argument('--max_steps', type=int, default=50000)
+    parser.add_argument('--eval_interval', type=int, default=100)
+    parser.add_argument('--max_steps_before_stop', type=int, default=3000)
+    parser.add_argument('--batch_size', type=int, default=5000)
+    parser.add_argument('--lm_batch_size', type=int, default=5000)
+    parser.add_argument('--max_grad_norm', type=float, default=1.0, help='Gradient clipping.')
+    parser.add_argument('--log_step', type=int, default=20, help='Print log every k steps.')
+    parser.add_argument('--save_dir', type=str, default='saved_models/dplm', help='Root dir for saving models.')
+    parser.add_argument('--save_name', type=str, default=None, help="File name to save the model")
+
+    parser.add_argument('--seed', type=int, default=1234)
+    parser.add_argument('--cuda', type=bool, default=torch.cuda.is_available())
+    parser.add_argument('--cpu', action='store_true', help='Ignore CUDA.')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+    if args.cpu:
+        args.cuda = False
+    elif args.cuda:
+        torch.cuda.manual_seed(args.seed)
+
+    print('Configuration:')
+    print('\n'.join('\t{:35} {}'.format(k + ':', str(v)) for k, v in sorted(dict(vars(args)).items())))
+    print()
+
+    args = vars(args)
+    print("Running parser in {} mode".format(args['mode']))
+
+    if args['mode'] == 'train':
+        train(args)
+    else:
+        evaluate(args)
+
+
+def train(args):
+    utils.ensure_dir(args['save_dir'])
+    model_file = args['save_dir'] + '/' + args['save_name'] if args['save_name'] is not None \
+        else '{}/{}_parser.pt'.format(args['save_dir'], args['shorthand'])
+
+    pretrain_file = '{}/{}.pretrain.pt'.format(args['save_dir'], args['shorthand'])
+    vec_file = utils.get_wordvec_file(args['wordvec_dir'], args['shorthand'])
+    pretrain = Pretrain(pretrain_file, vec_file)
+    use_cuda = args['cuda'] and not args['cpu']
+
+    lm_train_batch = LMDataLoader(args['lm_file'], args['lm_batch_size'], args, pretrain,
+                                  vocab=None, evaluation=False, cutoff=args['vocab_cutoff'])
+    vocab = lm_train_batch.vocab
+    dp_train_batch = DPDataLoader(args['train_file'], args['batch_size'], args, pretrain,
+                                  vocab=None, evaluation=False, cutoff=args['vocab_cutoff'])
+    vocab['deprel'] = dp_train_batch.vocab['deprel']
+    dp_train_batch = DPDataLoader(args['train_file'], args['batch_size'], args, pretrain,
+                                  vocab=vocab, evaluation=False, cutoff=args['vocab_cutoff'])
+    train_dev_batch = DPDataLoader(args['train_file'], args['batch_size'], args, pretrain, vocab=vocab, evaluation=True)
+    dev_batch = DPDataLoader(args['eval_file'], args['batch_size'], args, pretrain, vocab=vocab, evaluation=True)
+
+    lm_train_iter = iter(lm_train_batch)
+    dp_train_iter = iter(dp_train_batch)
+
+    # pred and gold path
+    system_pred_file = args['output_file']
+    gold_file = args['gold_file']
+
+    print("Training parser...")
+    trainer = Trainer(args=args, vocab=vocab, pretrain=pretrain, use_cuda=args['cuda'], weight_decay=args['wdecay'])
+    print()
+    print('Parameters that require grad:')
+    for p_name, p in trainer.model.named_parameters():
+        if p.requires_grad == True:
+            print('\t{:10}    {}'.format(p_name, p.size()))
+
+    global_step = 0
+    max_steps = args['max_steps']
+    dev_score_history = []
+    best_dev_preds = []
+    current_lr = args['lr']
+    global_start_time = time.time()
+    format_str = '{}: step {}/{}, loss = {:.6f} ({:.3f} sec/batch), dp_loss = {:.4f}, ppl = {:.2f}, lr: {:.6f}'
+
+    using_amsgrad = False
+    last_best_step = 0
+    # start training
+    log_loss = np.zeros(3)
+    train_loss = np.zeros(3)
+    while True:
+        do_break = False
+
+        try:
+            lm_batch = next(lm_train_iter)
+        except StopIteration:
+            lm_train_iter = iter(lm_train_batch)
+            lm_batch = next(lm_train_iter)
+        try:
+            dp_batch = next(dp_train_iter)
+        except StopIteration:
+            dp_train_iter = iter(dp_train_batch)
+            dp_batch = next(dp_train_iter)
+
+        start_time = time.time()
+        global_step += 1
+        dp_loss, lm_loss, loss = trainer.update(dp_batch, lm_batch, eval=False)  # update step
+        log_loss += np.array([lm_loss, dp_loss, loss])
+        train_loss += np.array([lm_loss, dp_loss, loss])
+        if global_step % args['log_step'] == 0:
+            duration = time.time() - start_time
+            log_loss = log_loss / args['log_step']
+            print(format_str.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), global_step,
+                                    max_steps, log_loss[2], duration, log_loss[1], np.exp(log_loss[0]), current_lr))
+            log_loss[:] = 0
+
+        if global_step % args['eval_interval'] == 0:
+            # eval on train
+            train_preds = []
+            for batch in train_dev_batch:
+                preds = trainer.predict(batch)
+                train_preds += preds
+
+            train_dev_batch.conll.set(['head', 'deprel'], [y for x in train_preds for y in x])
+            train_dev_batch.conll.write_conll(system_pred_file)
+            _, _, train_score = scorer.score(system_pred_file, args['train_file'])
+
+            # eval on dev
+            print("Evaluating on dev set...")
+            dev_preds = []
+            for batch in dev_batch:
+                preds = trainer.predict(batch)
+                dev_preds += preds
+
+            dev_batch.conll.set(['head', 'deprel'], [y for x in dev_preds for y in x])
+            dev_batch.conll.write_conll(system_pred_file)
+            _, _, dev_score = scorer.score(system_pred_file, gold_file)
+
+            train_loss = train_loss / args['eval_interval']  # avg loss per batch
+            print("step {}: train_score = {:.4f}, dev_score = {:.4f}".format(global_step, train_score, dev_score))
+            train_loss[:] = 0
+
+            # save best model
+            if len(dev_score_history) == 0 or dev_score > max(dev_score_history):
+                last_best_step = global_step
+                trainer.save(model_file)
+                print("new best model saved.")
+                best_dev_preds = dev_preds
+
+            dev_score_history += [dev_score]
+            print("")
+
+        if global_step - last_best_step >= args['max_steps_before_stop']:
+            if not using_amsgrad:
+                print("Switching to AMSGrad")
+                last_best_step = global_step
+                using_amsgrad = True
+                trainer.optimizer = optim.Adam(trainer.model.parameters(), amsgrad=True, lr=args['lr'], betas=(.9, args['beta2']), eps=1e-6)
+            else:
+                do_break = True
+                break
+
+        if global_step >= args['max_steps']:
+            do_break = True
+            break
+
+        if do_break:
+            break
+
+        # train_batch.reshuffle()
+
+    print("Training ended with {} steps.".format(global_step))
+
+    best_f, best_eval = max(dev_score_history) * 100, np.argmax(dev_score_history) + 1
+    print("Best dev F1 = {:.2f}, at iteration = {}".format(best_f, best_eval * args['eval_interval']))
+
+
+def evaluate(args):
+    # file paths
+    system_pred_file = args['output_file']
+    gold_file = args['gold_file']
+    model_file = args['save_dir'] + '/' + args['save_name'] if args['save_name'] is not None \
+        else '{}/{}_parser.pt'.format(args['save_dir'], args['shorthand'])
+    pretrain_file = '{}/{}.pretrain.pt'.format(args['save_dir'], args['shorthand'])
+
+    # load pretrain
+    pretrain = Pretrain(pretrain_file)
+
+    # load model
+    use_cuda = args['cuda'] and not args['cpu']
+    trainer = Trainer(pretrain=pretrain, model_file=model_file, use_cuda=use_cuda)
+    loaded_args, vocab = trainer.args, trainer.vocab
+
+    # load config
+    for k in args:
+        if k.endswith('_dir') or k.endswith('_file') or k in ['shorthand'] or k == 'mode':
+            loaded_args[k] = args[k]
+
+    # load data
+    print("Loading data with batch size {}...".format(args['batch_size']))
+    batch = DataLoader(args['eval_file'], args['batch_size'], loaded_args, pretrain, vocab=vocab, evaluation=True)
+
+    if len(batch) > 0:
+        print("Start evaluation...")
+        preds = []
+        for i, b in enumerate(batch):
+            preds += trainer.predict(b)
+    else:
+        # skip eval if dev data does not exist
+        preds = []
+
+    # write to file and score
+    batch.conll.set(['head', 'deprel'], [y for x in preds for y in x])
+    batch.conll.write_conll(system_pred_file)
+
+    if gold_file is not None:
+        _, _, score = scorer.score(system_pred_file, gold_file)
+
+        print("Parser score:")
+        print("{} {:.2f}".format(args['shorthand'], score * 100))
+
+if __name__ == '__main__':
+    main()
