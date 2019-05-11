4d66ee8a3c99ecbf85edafce05c7e9bb9fcce4f3
evanfeng <evanfeng@pku.edu.cn>
2019-05-11 13:22:31 +0000

    Add WeightDropLSTM

--- stanfordnlp/models/common/layers.py
+++ stanfordnlp/models/common/layers.py
@@ -1,6 +1,44 @@
 import torch
 import torch.nn as nn
-from stanfordnlp.models.common.weight_drop_lstm import WeightDropLSTM
+from torch.nn import Parameter
+
+
+def _weight_drop(module, weights, dropout):
+    """
+    Helper for `WeightDrop`.
+    """
+
+    for name_w in weights:
+        w = getattr(module, name_w)
+        # del module._parameters[name_w]
+        module.register_parameter(name_w + '_raw', Parameter(w))
+
+    original_module_forward = module.forward
+
+    def forward(*args, **kwargs):
+        for name_w in weights:
+            raw_w = getattr(module, name_w + '_raw')
+            # w = torch.nn.functional.dropout(raw_w, p=dropout, training=module.training)
+            # setattr(module, name_w, w)
+            module._parameters[name_w] = torch.nn.functional.dropout(raw_w, p=dropout, training=module.training)
+
+        return original_module_forward(*args)
+
+    setattr(module, 'forward', forward)
+
+
+class WeightDropLSTM(torch.nn.LSTM):
+    """
+    Wrapper around :class:`torch.nn.LSTM` that adds ``weight_dropout`` named argument.
+
+    Args:
+        weight_dropout (float): The probability a weight will be dropped.
+    """
+
+    def __init__(self, *args, weight_dropout=0.0, **kwargs):
+        super().__init__(*args, **kwargs)
+        weights = ['weight_hh_l' + str(i) for i in range(self.num_layers)]
+        _weight_drop(self, weights, weight_dropout)
 
 
 def dropout_mask(x, sz, p: float):
