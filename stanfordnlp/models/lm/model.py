d9a732f5a6b55da53f23e97d196ebe43f6cae39b
evanfeng <evanfeng@pku.edu.cn>
2019-05-11 13:27:28 +0000

    Fix lstm layers, Rename model

--- stanfordnlp/models/lm/model.py
+++ stanfordnlp/models/lm/model.py
@@ -10,49 +10,12 @@ from stanfordnlp.models.common.hlstm import HighwayLSTM
 from stanfordnlp.models.common.dropout import WordDropout
 from stanfordnlp.models.common.vocab import CompositeVocab
 from stanfordnlp.models.common.char_model import CharacterModel
-from stanfordnlp.models.common.weight_drop_lstm import WeightDropLSTM
+from stanfordnlp.models.common.layers import WeightDropLSTM
 from stanfordnlp.models.common.rnn_utils import reverse_padded_sequence
+from stanfordnlp.models.common.layers import MultiLayerLSTM
 
 
-def dropout_mask(x, sz, p: float):
-    "Return a dropout mask of the same type as `x`, size `sz`, with probability `p` to cancel an element."
-    return x.new(*sz).bernoulli_(1 - p).div_(1 - p)
-
-
-class RNNDropout(nn.Module):
-    "Dropout with probability `p` that is consistent on the seq_len dimension."
-
-    def __init__(self, p: float=0.5):
-        super().__init__()
-        self.p = p
-
-    def forward(self, x):
-        if not self.training or self.p == 0.:
-            return x
-        m = dropout_mask(x.data, (x.size(0), 1, x.size(2)), self.p)
-        return x * m
-
-
-class AWDLSTM(nn.Module):
-
-    def __init__(self, in_dim, hid_dim, out_dim, n_layers, dropout, weight_dropout):
-        super().__init__()
-        self.n_layers = n_layers
-        self.rnns = [WeightDropLSTM(in_dim if l == 0 else hid_dim, (hid_dim if l != n_layers - 1 else out_dim),
-                                    1, batch_first=True, weight_dropout=weight_dropout) for l in range(n_layers)]
-        self.rnns = nn.ModuleList(self.rnns)
-        self.hidden_dps = nn.ModuleList([RNNDropout(dropout) for l in range(n_layers)])
-
-    def forward(self, inputs):
-        raw_output = inputs
-        for l, (rnn, hid_dp) in enumerate(zip(self.rnns, self.hidden_dps)):
-            raw_output, _ = rnn(raw_output)
-            if l != self.n_layers - 1:
-                raw_output = hid_dp(raw_output)
-        return raw_output, None
-
-
-class HLSTMLanguageModel(nn.Module):
+class LSTMBiLM(nn.Module):
 
     def __init__(self, args, vocab, emb_matrix=None, share_hid=False):
         super().__init__()
@@ -118,10 +81,10 @@ class HLSTMLanguageModel(nn.Module):
             self.lstm_backward = WeightDropLSTM(input_size, self.args['hidden_dim'], self.args['num_layers'], batch_first=True, bidirectional=False,
                                                 dropout=self.args['dropout'], weight_dropout=self.args['rec_dropout'])
         elif args['lstm_type'] == 'awdlstm':
-            self.lstm_forward = AWDLSTM(input_size, self.args['hidden_dim'], self.args['word_emb_dim'], self.args['num_layers'],
-                                        dropout=self.args['dropout'], weight_dropout=self.args['rec_dropout'])
-            self.lstm_backward = AWDLSTM(input_size, self.args['hidden_dim'], self.args['word_emb_dim'], self.args['num_layers'],
-                                         dropout=self.args['dropout'], weight_dropout=self.args['rec_dropout'])
+            self.lstm_forward = MultiLayerLSTM(input_size, self.args['hidden_dim'], self.args['word_emb_dim'], self.args['num_layers'],
+                                               dropout=self.args['dropout'], weight_dropout=self.args['rec_dropout'])
+            self.lstm_backward = MultiLayerLSTM(input_size, self.args['hidden_dim'], self.args['word_emb_dim'], self.args['num_layers'],
+                                                dropout=self.args['dropout'], weight_dropout=self.args['rec_dropout'])
         else:
             raise ValueError('LSTM type not supported')
 
