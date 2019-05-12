83c6120176d09d9562d1650c4daf8bfc30fa388d
evanfeng <evanfeng@pku.edu.cn>
2019-05-12 12:09:51 +0000

    Fix args order, Add _set_weights for param access

--- stanfordnlp/models/common/layers.py
+++ stanfordnlp/models/common/layers.py
@@ -10,7 +10,6 @@ def _weight_drop(module, weights, dropout):
 
     for name_w in weights:
         w = getattr(module, name_w)
-        # del module._parameters[name_w]
         module.register_parameter(name_w + '_raw', Parameter(w))
 
     original_module_forward = module.forward
@@ -18,8 +17,6 @@ def _weight_drop(module, weights, dropout):
     def forward(*args, **kwargs):
         for name_w in weights:
             raw_w = getattr(module, name_w + '_raw')
-            # w = torch.nn.functional.dropout(raw_w, p=dropout, training=module.training)
-            # setattr(module, name_w, w)
             module._parameters[name_w] = torch.nn.functional.dropout(raw_w, p=dropout, training=module.training)
 
         return original_module_forward(*args)
@@ -69,16 +66,26 @@ class MultiLayerLSTM(nn.Module):
     A multi-layer LSTM with specified output dimension.
     """
 
-    def __init__(self, input_size, hidden_size, output_size, num_layers,
+    def __init__(self, input_size, hidden_size, num_layers, output_size=None,
                  bidirectional=False, dropout=0, weight_dropout=0, batch_first=True):
         super().__init__()
         self.num_layers = num_layers
+        if output_size is None:
+            output_size = hidden_size
         self.rnns = [WeightDropLSTM(input_size if l == 0 else hidden_size,
                                     hidden_size if l != num_layers - 1 else output_size,
                                     1, bidirectional=bidirectional,
                                     batch_first=batch_first, weight_dropout=weight_dropout) for l in range(num_layers)]
         self.rnns = nn.ModuleList(self.rnns)
         self.hidden_dps = nn.ModuleList([RNNDropout(dropout) for l in range(num_layers)])
+        self._set_weights()
+
+    def _set_weights(self):
+        w_names = ['weight_ih_l{}', 'weight_hh_l{}', 'bias_ih_l{}', 'bias_hh_l{}', 'weight_hh_l{}_raw']
+        for l in range(self.num_layers):
+            for w_name in w_names:
+                w = getattr(self.rnns[l], w_name.format(0))
+                setattr(self, w_name.format(l), w)
 
     def forward(self, inputs):
         raw_output = inputs
