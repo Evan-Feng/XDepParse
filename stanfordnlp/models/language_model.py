105a37cbb83ec44bf8be5c1b6990d8b3ef6373eb
evanfeng <evanfeng@pku.edu.cn>
2019-05-13 09:47:08 +0000

    Default max_steps_before_stop to 10000

--- stanfordnlp/models/language_model.py
+++ stanfordnlp/models/language_model.py
@@ -73,7 +73,7 @@ def parse_args():
 
     parser.add_argument('--max_steps', type=int, default=50000)
     parser.add_argument('--eval_interval', type=int, default=100)
-    parser.add_argument('--max_steps_before_stop', type=int, default=3000)
+    parser.add_argument('--max_steps_before_stop', type=int, default=10000)
     parser.add_argument('--batch_size', type=int, default=5000)
     parser.add_argument('--eval_batch_size', type=int, default=2000)
     parser.add_argument('--max_grad_norm', type=float, default=1.0, help='Gradient clipping.')
