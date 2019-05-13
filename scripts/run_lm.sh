880df2f0cb3471e76aaa5f803a9f209cecd1cb24
evanfeng <evanfeng@pku.edu.cn>
2019-05-13 11:44:48 +0000

    Update run_lm.sh

--- scripts/run_lm.sh
+++ scripts/run_lm.sh
@@ -11,7 +11,7 @@ word_dropout=0.1
 wdecay=1e-6
 lr=3e-3
 rec_dropout=0.5
-clip=0.25
+clip=0.5
 
 echo "Running language model with $args..."
 
@@ -21,9 +21,10 @@ python -u -m stanfordnlp.models.language_model \
     --train_file data/train_src.tsv data/train_trg.tsv \
     --eval_file data/test_src.tsv data/test_trg.tsv \
     --wordvec_dir $WORDVEC_DIR \
-    --batch_size $batch_size --mode train --eval_interval 400 \
-    --lstm_type awdlstm --num_layers 3 --hidden_dim 1150 \
+    --batch_size $batch_size --eval_batch_size 2000 --eval_interval 400 \
+    --lstm_type wdlstm --num_layers 3 --hidden_dim 800 \
     --lemma_emb_dim 0 --word_emb_dim 300 --transformed_dim 0 --char_emb_dim 0  \
+    --tie_softmax \
     --lr $lr --wdecay $wdecay \
     --dropout $dropout --rec_dropout $rec_dropout --word_dropout $word_dropout \
     --max_grad_norm $clip \
