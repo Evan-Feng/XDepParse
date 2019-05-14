d0a48dee985d80a0ff056a453ba30c799790cd7d
evanfeng <evanfeng@pku.edu.cn>
2019-05-14 02:48:05 +0000

    Remove --deep_biaff_hidden_dim --composite_deep_biaff_hidden_dim

--- stanfordnlp/models/language_model.py
+++ stanfordnlp/models/language_model.py
@@ -48,8 +48,6 @@ def parse_args():
 
     parser.add_argument('--hidden_dim', type=int, default=400)
     parser.add_argument('--char_hidden_dim', type=int, default=400)
-    parser.add_argument('--deep_biaff_hidden_dim', type=int, default=400)
-    parser.add_argument('--composite_deep_biaff_hidden_dim', type=int, default=100)
     parser.add_argument('--word_emb_dim', type=int, default=75)
     parser.add_argument('--char_emb_dim', type=int, default=100)
     parser.add_argument('--tag_emb_dim', type=int, default=50)
